ed8c63e5a1b4348f568812af347748892edfb91f
Darshan Desai <de.darshan@yahoo.com>
2013-12-02 23:54:08 -0800

    Initial check in

---
+++ CCM/CCM.DAL/NHibernateSessionProvider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Cfg;
+using FluentNHibernate;
+
+namespace CCM.DAL
+{
+    public class NHibernateSessionProvider
+    {
+        private static ISessionFactory sessionFactory;
+
+        public static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (sessionFactory == null)
+                    sessionFactory = SetSessionFactory();
+                return sessionFactory;
+            }
+        }
+
+        public static ISession OpenSession()
+        {
+            ISession s = SessionFactory.OpenSession();
+            return s;
+        }
+
+        public static ISessionFactory SetSessionFactory()
+        {
+            // Set default to SQL 2005 database                            
+            var config = Fluently.Configure()
+                //.Database(MsSqlConfiguration.MsSql2008.ConnectionString(x => x.("EmployeeConnectConnectionString")))
+                                         .Database(MsSqlConfiguration.MsSql2008.ConnectionString("Integrated Security=True;Initial Catalog=CCM;Data Source=DARSHANDESAI-PC\\SQLEXPRESS"))
+                                         .ExposeConfiguration(c => c.SetProperty(NHibernate.Cfg.Environment.ProxyFactoryFactoryClass, typeof(NHibernate.ByteCode.Castle.ProxyFactoryFactory).AssemblyQualifiedName))
+                                         .ExposeConfiguration(c => c.SetProperty(NHibernate.Cfg.Environment.ReleaseConnections, "on_close"))
+                                         .BuildConfiguration();
+
+            var persistenceModel = new PersistenceModel();
+            //persistenceModel.AddMappingsFromAssembly(typeof(EmployeeMap).Assembly);
+            //persistenceModel.AddMappingsFromAssembly(typeof(RequestStatusMap).Assembly);
+            persistenceModel.Configure(config);
+
+            sessionFactory = config.BuildSessionFactory();
+
+            return sessionFactory;
+        }
+
+    }
+}
---
+++ CCM/CCM.DAL/RepositoryBase.cs
@@ -0,0 +1,46 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Linq;
+
+namespace CCM.DAL
+{
+    public class RepositoryBase<T> : NHibernateSessionProvider where T : class
+    {
+        public virtual T Get(int id)
+        {
+            using (ISession session = NHibernateSessionProvider.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                var a = session.Get<T>(id);
+                return a;
+            }
+
+        }
+
+        public virtual IQueryable<T> GetQuery()
+        {
+            using (ISession session = NHibernateSessionProvider.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                var query = session.Query<T>();
+                return query;
+            }
+        }
+
+        public virtual void Save(T obj)
+        {
+            using (ISession session = NHibernateSessionProvider.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Save(obj);
+                transaction.Commit();
+            }
+
+        }
+
+    }
+}
